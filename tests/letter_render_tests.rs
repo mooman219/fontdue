bf34b30b72e8b11407e02fe49f97010efe785f30
Robert McLaughlin <robert@sparkk.us>
2019-10-08 17:36:29 -0700

    run rustfmt

--- tests/letter_render_tests.rs
+++ tests/letter_render_tests.rs
@@ -1,15 +1,13 @@
+use fontdue;
 use std::assert;
 use std::fs;
 use std::path::Path;
-use fontdue;
 
 const CHAR_SIZE: f32 = 17.0;
 
 // Loads a font by file name from the `resources/` directory
 fn load_font(font_file: String) -> fontdue::Font {
-    let file_path = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("resources")
-        .join(font_file);
+    let file_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("resources").join(font_file);
     let font = fs::read(file_path).unwrap();
     return fontdue::Font::from_bytes(font).unwrap();
 }
@@ -23,13 +21,11 @@ fn check_best_guess_rasterization((metrics, bitmap): (fontdue::Metrics, Vec<u8>)
     assert_eq!(
         metrics.width * metrics.height,
         bitmap.len(),
-        "bitmap must match dimensions for character '{}'", rendered_char
+        "bitmap must match dimensions for character '{}'",
+        rendered_char
     );
     // Ensure that somewhere in the bitmap is a non-zero pixel
-    assert!(
-        bitmap.iter().any(|b| *b != 0),
-        "some pixel must be non-zero when rendering '{}'", rendered_char
-    );
+    assert!(bitmap.iter().any(|b| *b != 0), "some pixel must be non-zero when rendering '{}'", rendered_char);
 }
 
 #[test]
