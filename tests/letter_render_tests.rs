808366ce77a686d6f74f8fd7c992b96f7d26791b
Robert McLaughlin <robert@sparkk.us>
2019-10-15 16:36:07 -0700

    correct tests to use include_bytes!

--- tests/letter_render_tests.rs
+++ tests/letter_render_tests.rs
@@ -1,16 +1,11 @@
 use fontdue;
 use std::assert;
-use std::fs;
-use std::path::Path;
 
 const CHAR_SIZE: f32 = 17.0;
 
-// Loads a font by file name from the `resources/` directory
-fn load_font(font_file: String) -> fontdue::Font {
-    let file_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("resources").join(font_file);
-    let font = fs::read(file_path).unwrap();
-    return fontdue::Font::from_bytes(font).unwrap();
-}
+const ROBOTO_REGULAR_TTF: &[u8] = include_bytes!("../resources/Roboto-Regular.ttf");
+const ROBOTO_MONO_REGULAR_TTF: &[u8] = include_bytes!("../resources/RobotoMono-Regular.ttf");
+const LIBERATION_SERIF_REGULAR: &[u8] = include_bytes!("../resources/LiberationSerif-Regular.ttf"); 
 
 // Performs some basic asserts on the rasterization output.
 fn check_best_guess_rasterization((metrics, bitmap): (fontdue::Metrics, Vec<u8>), rendered_char: char) {
@@ -30,7 +25,7 @@ fn check_best_guess_rasterization((metrics, bitmap): (fontdue::Metrics, Vec<u8>)
 
 #[test]
 fn render_roboto_characters() {
-    let font = load_font(String::from("Roboto-Regular.ttf"));
+    let font = fontdue::Font::from_bytes(ROBOTO_REGULAR_TTF).unwrap();
 
     for chr in &['a', '1', '2', '#', '?', '█', '▒', '¾', 'æ'] {
         check_best_guess_rasterization(font.rasterize(*chr, CHAR_SIZE), *chr);
@@ -39,7 +34,7 @@ fn render_roboto_characters() {
 
 #[test]
 fn render_roboto_mono_characters() {
-    let font = load_font(String::from("RobotoMono-Regular.ttf"));
+    let font = fontdue::Font::from_bytes(ROBOTO_MONO_REGULAR_TTF).unwrap();
 
     for chr in &['a', '1', '2', '#', '?', 'æ'] {
         check_best_guess_rasterization(font.rasterize(*chr, CHAR_SIZE), *chr);
@@ -48,7 +43,7 @@ fn render_roboto_mono_characters() {
 
 #[test]
 fn render_liberation_serif_characters() {
-    let font = load_font(String::from("LiberationSerif-Regular.ttf"));
+    let font = fontdue::Font::from_bytes(LIBERATION_SERIF_REGULAR).unwrap();
 
     for chr in &['a', '1', '2', '#', '?', '█', '▒', '¾', 'æ'] {
         check_best_guess_rasterization(font.rasterize(*chr, CHAR_SIZE), *chr);
