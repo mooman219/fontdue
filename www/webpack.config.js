fc211a1bfb461b45e459659f1d3755a61413a52f
Wesley Wigham <wwigham@gmail.com>
2020-09-04 01:06:05 -0700

    Initial web comparison drop

---
+++ www/webpack.config.js
@@ -0,0 +1,14 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "../docs"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin(['index.html'])
+  ],
+};
