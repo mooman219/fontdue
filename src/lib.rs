4cc3586fc3790b5d08a1435cd9d1c822adb2301c
Jonathan Spira <jjspira@gmail.com>
2021-03-26 18:38:47 -0700

    updating fondue to stop yelling at me. This lib is very annoying.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
 //! Fontdue is a font parser, rasterizer, and layout tool.
 //!
 //! This is a #![no_std] crate, but still requires the alloc crate.
-
 #![no_std]
 #![allow(dead_code)]
+#![allow(
+    clippy::excessive_precision,
+    clippy::approx_constant,
+    clippy::float_cmp,
+    clippy::needless_bool,
+    clippy::upper_case_acronyms,
+    clippy::many_single_char_names,
+    clippy::wildcard_in_or_patterns,
+    clippy::needless_return,
+    clippy::transmute_int_to_float,
+    clippy::transmute_float_to_int,
+    clippy::ptr_arg,
+    clippy::transmute_ptr_to_ptr,
+    clippy::let_and_return,
+    clippy::redundant_slicing,
+)]
 
 extern crate alloc;
 
