25cdd0bf9dddede740224dc016f3fa93ec52b659
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2022-01-12 19:45:12 +0100

    partial fix for #98

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,10 @@
 #![allow(clippy::style)]
 #![allow(clippy::complexity)]
 
+// Partial fix for https://github.com/mooman219/fontdue/issues/98
+#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), feature = "simd", not(target_feature = "sse")))]
+compile_error!("on x86_64 the `simd` feature only compiles, if SSE is enabled!");
+
 extern crate alloc;
 
 mod font;
