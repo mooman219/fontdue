04260119e355f881a1e97466cd69b035067b6c80
mooman219 <mooman219@gmail.com>
2022-02-25 01:55:42 -0800

    Mistyping documentation is my passion

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- `LinePosition` doc typo.
 
 ## [0.7.0] - 2022-02-25
 ### Added
@@ -12,6 +14,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - `simd` flag, enabled by default. Leverages simd functions. This was implicitly always enabled prior.
 - `parallel` flag, disabled by default. Uses `std` + `rayon` to thread font loading.
 - `Font.chars()` gets all valid unicode codepoints that have mappings to glyph geometry in the font.
-- `LinePositioned` holds various metadata on positioned lines computed during layout.
+- `LinePosition` holds various metadata on positioned lines computed during layout.
 ### Changed
-- `Layout.lines()` returns a `Option<Vec<LinePositioned>>` now instead of a line count.
+- `Layout.lines()` returns a `Option<Vec<LinePosition>>` now instead of a line count.
--- src/layout.rs
+++ src/layout.rs
@@ -201,14 +201,14 @@ pub struct LinePosition {
     /// positive. If there are multiple styles on this line, this their max value.
     pub max_ascent: f32,
     /// The lowest point that any glyph in the font extends to below the baseline. Typically
-    /// negative. If there are multiple styles on this line, this their max value.
+    /// negative. If there are multiple styles on this line, this their min value.
     pub min_descent: f32,
     /// The gap to leave between the descent of one line and the ascent of the next. This is of
     /// course only a guideline given by the font's designers. If there are multiple styles on this
     /// line, this their max value.
     pub max_line_gap: f32,
     /// A precalculated value for the of the line depending. It's calculated by: ascent - descent +
-    /// line_gap. If there are multiple styles on this line, this their min value.
+    /// line_gap. If there are multiple styles on this line, this their max value.
     pub max_new_line_size: f32,
     /// The GlyphPosition index of the first glyph in the line.
     pub line_start: usize,
