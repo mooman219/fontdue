c4649058fe36b450596e000dc6d10f02c266f058
mooman219 <mooman219@gmail.com>
2022-03-07 01:39:36 -0800

    Remove some unsafe

--- src/platform/float/mod.rs
+++ src/platform/float/mod.rs
@@ -18,40 +18,34 @@ pub use get_bitmap::*;
 pub use sqrt::*;
 pub use trunc::*;
 
-use core::mem::transmute;
-
 /// Sets the high bit 0x80000000 on a float.
 #[inline(always)]
 pub fn abs(value: f32) -> f32 {
-    unsafe { transmute::<u32, f32>(transmute::<f32, u32>(value) & 0x7fffffff) }
+    f32::from_bits(value.to_bits() & 0x7fffffff)
 }
 
 /// Checks if the high bit 0x80000000 is set on a float.
 #[inline(always)]
 pub fn is_negative(value: f32) -> bool {
-    unsafe { transmute::<f32, u32>(value) >= 0x80000000 }
+    value.to_bits() >= 0x80000000
 }
 
 /// Checks if the high bit 0x80000000 is not set on a float.
 #[inline(always)]
 pub fn is_positive(value: f32) -> bool {
-    unsafe { transmute::<f32, u32>(value) < 0x80000000 }
+    value.to_bits() < 0x80000000
 }
 
 /// Inverts the high bit 0x80000000 on a float.
 #[inline(always)]
 pub fn flipsign(value: f32) -> f32 {
-    unsafe { transmute::<u32, f32>(transmute::<f32, u32>(value) ^ 0x80000000) }
+    f32::from_bits(value.to_bits() ^ 0x80000000)
 }
 
 /// Assigns the high bit 0x80000000 on the sign to the value.
 #[inline(always)]
 pub fn copysign(value: f32, sign: f32) -> f32 {
-    unsafe {
-        transmute::<u32, f32>(
-            (transmute::<f32, u32>(value) & 0x7fffffff) | (transmute::<f32, u32>(sign) & 0x80000000),
-        )
-    }
+    f32::from_bits((value.to_bits() & 0x7fffffff) | (sign.to_bits() & 0x80000000))
 }
 
 #[inline(always)]
